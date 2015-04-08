fe175b0a3b8e47f0a051a64d3ac4e3396b384b75
Ivano Cortesini <ivano.cortesini@gmail.com>
2015-04-08 08:30:03 +0200

    ...

---
+++ fenix-D3P-core/src/main/java/org/fao/fenix/d3p/dao/StorageFactory.java
@@ -0,0 +1,18 @@
+package org.fao.fenix.d3p.dao;
+
+import org.fao.fenix.d3s.cache.D3SCache;
+import org.fao.fenix.d3s.cache.manager.CacheManager;
+import org.fao.fenix.d3s.cache.manager.CacheManagerFactory;
+import org.fao.fenix.d3s.cache.storage.dataset.DatasetStorage;
+
+import javax.inject.Inject;
+
+public class StorageFactory {
+    private @Inject CacheManagerFactory cacheFactory;
+
+
+    public DatasetStorage getInstance() throws Exception {
+        CacheManager cacheManager = cacheFactory.getInstance(D3SCache.fixed.name());
+        return (DatasetStorage)cacheManager.getStorage();
+    }
+}
--- fenix-D3P-core/src/main/java/org/fao/fenix/d3p/services/Processes.java
+++ fenix-D3P-core/src/main/java/org/fao/fenix/d3p/services/Processes.java
@@ -1,8 +1,15 @@
 package org.fao.fenix.d3p.services;
 
 import org.fao.fenix.commons.msd.dto.data.ResourceProxy;
+import org.fao.fenix.commons.msd.dto.templates.export.combined.dataset.MetadataDSD;
 import org.fao.fenix.commons.process.dto.Process;
+import org.fao.fenix.d3p.dao.StorageFactory;
+import org.fao.fenix.d3s.cache.dto.StoreStatus;
+import org.fao.fenix.d3s.cache.manager.CacheManagerFactory;
+import org.fao.fenix.d3s.cache.storage.dataset.DatasetStorage;
+import org.fao.fenix.d3s.msd.services.spi.Resources;
 
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
@@ -13,6 +20,8 @@ import javax.ws.rs.core.MediaType;
 @Consumes(MediaType.APPLICATION_JSON)
 public class Processes {
     private @Context HttpServletRequest httpRequest;
+    private @Inject StorageFactory storageFactory;
+    private @Inject Resources resourcesService;
 
     /**
      * Apply a process workflow to a domain or a cached resource.
@@ -24,7 +33,7 @@ public class Processes {
      */
     @POST
     @Path("{uid}")
-    public ResourceProxy apply(@PathParam("uid") String uid, Process[] flow) {
+    public ResourceProxy apply(@PathParam("uid") String uid, Process[] flow) throws Exception {
         return apply(uid,null,flow);
     }
 
@@ -39,9 +48,25 @@ public class Processes {
      */
     @POST
     @Path("{uid}/{version}")
-    public ResourceProxy apply(@PathParam("uid") String uid, @PathParam("version") String version, Process[] flow) {
+    public ResourceProxy apply(@PathParam("uid") String uid, @PathParam("version") String version, Process[] flow) throws Exception {
+        String id = getId(uid, version);
+        DatasetStorage storage = storageFactory.getInstance();
+        StoreStatus resourceStatus = id!=null && storage!=null ? storage.loadMetadata(id) : null;
+
+        MetadataDSD metadata = (MetadataDSD)resourcesService.getMetadataByUID(uid, version, true, true, true);
+
+
+
         return null;
     }
 
+
+
+    private String getId(String uid, String version) {
+        if (uid!=null)
+            return version!=null ? uid+'|'+version : uid;
+        else
+            return null;
+    }
 
 }
