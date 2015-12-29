a05a2cb6435336f50b17a92a0b470cfe7bc1f774
Ivano Cortesini <ivano.cortesini@gmail.com>
2015-12-29 17:46:44 +0100

    ...

--- fenix-D3P-core/src/main/java/org/fao/fenix/d3p/cache/CacheFactory.java
+++ fenix-D3P-core/src/main/java/org/fao/fenix/d3p/cache/CacheFactory.java
@@ -1,10 +1,8 @@
 package org.fao.fenix.d3p.cache;
 
-import org.fao.fenix.commons.msd.dto.full.DSD;
-import org.fao.fenix.commons.msd.dto.full.DSDCache;
-import org.fao.fenix.commons.msd.dto.full.DSDDataset;
-import org.fao.fenix.commons.msd.dto.full.MeIdentification;
-import org.fao.fenix.d3s.cache.D3SCache;
+import org.fao.fenix.commons.msd.dto.full.*;
+import org.fao.fenix.commons.msd.dto.type.RepresentationType;
+import org.fao.fenix.d3s.cache.manager.CacheResourceType;
 import org.fao.fenix.d3s.cache.manager.CacheManager;
 import org.fao.fenix.d3s.cache.manager.CacheManagerFactory;
 
@@ -20,6 +18,19 @@ public class CacheFactory {
         DSDCache cacheInfo = dsd!=null ? dsd.getCache() : null;
         String managerName = cacheInfo!=null ? cacheInfo.getManager() : null;
         String storageName = cacheInfo!=null ? cacheInfo.getStorage() : null;
-        return cacheManagerFactory.getInstance(managerName!=null ? managerName : "dataset", storageName!=null ? storageName : "h2");
+        return cacheManagerFactory.getInstance(getCacheType(metadata), managerName!=null ? managerName : "dataset", storageName!=null ? storageName : "h2");
+    }
+
+    private CacheResourceType getCacheType(MeIdentification metadata) {
+        MeContent content = metadata!=null ? metadata.getMeContent() : null;
+        RepresentationType type = content!=null ? content.getResourceRepresentationType() : null;
+        if (type!=null)
+            switch (type) {
+                case dataset: return CacheResourceType.dataset;
+                case codelist: return CacheResourceType.codelist;
+                case document: return CacheResourceType.document;
+                default: return CacheResourceType.binary;
+            }
+        return CacheResourceType.dataset;
     }
 }
