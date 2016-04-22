31a68208303f2d842799db3681d22d440e525ef4
fabriziofenix <fabrizio.castelli@fao.org>
2016-04-22 17:12:05 +0200

    ...

--- fenix-D3P-core/src/main/java/org/fao/fenix/d3p/process/impl/Join.java
+++ fenix-D3P-core/src/main/java/org/fao/fenix/d3p/process/impl/Join.java
@@ -7,6 +7,8 @@ import org.fao.fenix.d3p.process.dto.JoinParams;
 import org.fao.fenix.d3p.process.impl.join.JoinLogic;
 import org.fao.fenix.d3p.process.impl.join.JoinLogicFactory;
 import org.fao.fenix.d3p.process.type.ProcessName;
+import org.fao.fenix.d3s.cache.storage.dataset.DatasetStorage;
+
 import javax.ws.rs.BadRequestException;
 
 @ProcessName("join")
@@ -37,10 +39,15 @@ public class Join extends org.fao.fenix.d3p.process.Process<JoinParams>{
         if(sourceSteps== null || sourceSteps.length <2){
             throw new BadRequestException("join should have at least two sid");
         }
+        DatasetStorage storage = sourceSteps[0].getStorage();
+
         for(Step step: sourceSteps){
             StepType type= step.getType();
-            if (type==null || (type != StepType.table ))
-                throw new UnsupportedOperationException("filter process can be applied only on a table or an other select query");
+            if (type==null || ((type != StepType.table ) && ((type != StepType.query) )))
+                throw new UnsupportedOperationException("join process can only be applied on a table or on other select query");
+            if (!step.getStorage().equals(storage))
+                throw new UnsupportedOperationException("join process can be applied only on dataset deployed on the same storage");
+
         }
     }
 
--- fenix-D3P-core/src/main/java/org/fao/fenix/d3p/process/impl/join/logic/ManualJoin.java
+++ fenix-D3P-core/src/main/java/org/fao/fenix/d3p/process/impl/join/logic/ManualJoin.java
@@ -1,21 +1,16 @@
 package org.fao.fenix.d3p.process.impl.join.logic;
 
-import com.sun.corba.se.spi.logging.CORBALogDomains;
-import org.fao.fenix.commons.msd.dto.full.DSD;
 import org.fao.fenix.commons.msd.dto.full.DSDColumn;
-import org.fao.fenix.commons.msd.dto.templates.identification.DSDDataset;
 import org.fao.fenix.d3p.dto.Step;
 import org.fao.fenix.d3p.process.dto.JoinParameter;
 import org.fao.fenix.d3p.process.dto.JoinParams;
 import org.fao.fenix.d3p.process.dto.JoinTypes;
+import org.fao.fenix.d3p.process.impl.Join;
+import org.fao.fenix.d3p.process.impl.Page;
 import org.fao.fenix.d3p.process.impl.join.JoinLogic;
 
 import javax.ws.rs.BadRequestException;
-import javax.ws.rs.core.Link;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class ManualJoin implements JoinLogic {
 
@@ -32,10 +27,9 @@ public class ManualJoin implements JoinLogic {
         // check that parameters follows the sid
 
         validate(sourceStep);
+        createDSDColumns(sourceStep);
 
         StringBuilder query;
-
-
 
 
         return new Step[0];
@@ -43,7 +37,7 @@ public class ManualJoin implements JoinLogic {
 
     private void validate(Step... steps) {
         if (this.params.getJoins().size() == steps.length) {
-            Iterator<Collection<JoinParameter>> it =  this.params.getJoins().iterator();
+            Iterator<Collection<JoinParameter>> it = this.params.getJoins().iterator();
             int sizeFirst = it.next().size();
             while (it.hasNext()) {
                 if (sizeFirst != it.next().size()) {
@@ -55,46 +49,104 @@ public class ManualJoin implements JoinLogic {
         }
     }
 
-    private List<DSDColumn> createDSDColumns (Step... sourceStep) {
-
+    private List<DSDColumn> createDSDColumns(Step... sourceStep) {
 
         // create key Columns
+        Map<Integer, Set<Integer>> positionKeyColumnsForDataset = new HashMap<Integer, Set<Integer>>();
         List<DSDColumn> resultColumns = new ArrayList<DSDColumn>();
-        fillKeyColumns(resultColumns,sourceStep);
-
-        
+        fillKeyColumns(resultColumns, positionKeyColumnsForDataset, sourceStep);
+        fillOtherColumns(resultColumns, positionKeyColumnsForDataset, sourceStep);
+        return null;
 
     }
 
-    private void fillKeyColumns (List<DSDColumn> columns, Step... sources) {
+    private void fillKeyColumns(List<DSDColumn> columns, Map<Integer, Set<Integer>> positionKeyColumns, Step... sources) {
 
-        JoinParameter[][] parameters = (JoinParameter[][]) this.params.getJoins().toArray();
-        for(int i=0, keyColumns = parameters[0].length; i<keyColumns; i++) {
+        ArrayList<Collection<JoinParameter>> parameters = (ArrayList<Collection<JoinParameter>>) this.params.getJoins();
+        for (int i = 0, keyColumns = ((ArrayList<JoinParameter>) parameters.get(0)).size(); i < keyColumns; i++) {
+            boolean idFound = false;
             // search id for each dataset
-            for(int j=0, sidLength = parameters.length; j<sidLength; j++){
-                if(parameters[j][i].getType() == JoinTypes.id) {
-                    DSDColumn column = find(parameters[j][i].getValue().toString(), sources[j].getDsd());
-                    if(column!=null)
-                        columns.add(column);
+            for (int j = 0, sidLength = parameters.size(); j < sidLength; j++) {
+                ArrayList<JoinParameter> parameterList = (ArrayList<JoinParameter>) parameters.get(j);
+                if (parameterList.get(i).getType() == JoinTypes.id) {
+                    if (idFound == false) {
+                        DSDColumn column = find(parameterList.get(i).getValue().toString(), sources[j].getDsd());
+                        if (column != null) {
+                            setPositionKey(positionKeyColumns, j, i);
+                            columns.add(column);
+                            idFound = true;
+                        }
+                    } else {
+                        setPositionKey(positionKeyColumns, j, i);
+                    }
                 }
             }
         }
 
-        if (columns.size() <=0)
+        if (columns.size() <= 0)
             throw new BadRequestException("wrong configuration for join parameters:id parameters does not exists");
+    }
+
+    private void setPositionKey(Map<Integer, Set<Integer>> positions, int key, int value) {
+        Set<Integer> values = (HashSet<Integer>) positions.get(key);
+        values = values == null ? new HashSet<Integer>() : values;
+        values.add(value);
+        positions.put(key, values);
+
     }
 
 
     private DSDColumn find(String columnID, org.fao.fenix.commons.msd.dto.full.DSDDataset dsdDataset) {
-        for(DSDColumn column: dsdDataset.getColumns()){
-            if(column.getId().equals(columnID))
+        for (DSDColumn column : dsdDataset.getColumns()) {
+            if (column.getId().equals(columnID))
                 return column;
         }
-        return null;
+        throw new BadRequestException("wrong configuration for join parameters: id does not exists into that dataset");
+    }
+
+    private void fillOtherColumns(List<DSDColumn> columns, Map<Integer, Set<Integer>> positionKeyColumns, Step... sources) {
+
+        Map<Integer, Set<String>> valuesToBeShown = (areValuesParameters(sources)) ? createMapValues() : null;
+        for (int i = 0, sidSize = sources.length; i < sidSize; i++) {
+            org.fao.fenix.commons.msd.dto.full.DSDDataset dataset = sources[i].getDsd();
+            for (int j = 0, datasetSize = dataset.getColumns().size(); j < datasetSize; j++) {
+                // if it is not duplicated, add
+                if (!isADuplicateColumn(i, j, positionKeyColumns) ||
+                        (valuesToBeShown != null && valuesToBeShown.get(i).contains(((ArrayList<DSDColumn>) dataset.getColumns()).get(j).getId())) ) {
+                        columns.add(((ArrayList<DSDColumn>) dataset.getColumns()).get(j));
+                }
+            }
+        }
+    }
+
+    private boolean isADuplicateColumn(int row, int column, Map<Integer, Set<Integer>> positionKeyColumns) {
+        return positionKeyColumns.get(row) != null && positionKeyColumns.get(row).contains(column);
     }
 
 
+    private boolean areValuesParameters(Step... source) {
+        if (this.params.getValues() != null && this.params.getValues().size() > 0) {
+            if (source.length == this.params.getValues().size()) {
+                return true;
 
+            } else {
+                throw new BadRequestException("wrong configuration for values parameter : number of values should follow the number of dataset");
+            }
+        } else {
+            return false;
+        }
+    }
 
+    private Map<Integer, Set<String>> createMapValues() {
+        Map<Integer, Set<String>> result = new HashMap<Integer, Set<String>>();
+        ArrayList<Collection<String>> values = (ArrayList<Collection<String>>) this.params.getValues();
+        for (int i = 0, size = values.size(); i < size; i++) {
+            Set<String> ids = new HashSet<String>();
+            for (String s : values.get(i))
+                ids.add(s);
+            result.put(i, ids);
+        }
+        return result;
+    }
 
 }
