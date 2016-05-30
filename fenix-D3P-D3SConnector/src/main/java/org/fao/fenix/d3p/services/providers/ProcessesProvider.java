92b5e31b1a8c3c3902424c7acc0a71449051427b
Ivano Cortesini <ivano.cortesini@gmail.com>
2016-05-30 10:58:10 +0200

    ...

--- fenix-D3P-D3SConnector/src/main/java/org/fao/fenix/d3p/services/providers/ProcessesProvider.java
+++ fenix-D3P-D3SConnector/src/main/java/org/fao/fenix/d3p/services/providers/ProcessesProvider.java
@@ -4,9 +4,13 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import org.fao.fenix.commons.process.dto.Process;
 import org.fao.fenix.commons.utils.JSONUtils;
+import org.fao.fenix.commons.utils.Order;
+import org.fao.fenix.commons.utils.Page;
 import org.fao.fenix.d3p.process.ProcessFactory;
+import org.fao.fenix.d3s.server.dto.DatabaseStandards;
 
 import javax.inject.Inject;
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
@@ -17,11 +21,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
+import java.util.Collection;
+import java.util.LinkedList;
 
 @Provider
 @Consumes(MediaType.APPLICATION_JSON)
 public class ProcessesProvider extends JsonProvider implements MessageBodyReader<Process[]> {
     @Inject ProcessFactory processFactory;
+    @Inject DatabaseStandards additionalParameters;
 
     @Override
     public boolean isReadable(Class<?> aClass, Type type, Annotation[] annotations, MediaType mediaType) {
@@ -31,19 +38,26 @@ public class ProcessesProvider extends JsonProvider implements MessageBodyReader
     @Override
     public Process[] readFrom(Class<Process[]> aClass, Type type, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, String> multivaluedMap, InputStream inputStream) throws IOException, WebApplicationException {
         try {
-            return decodeProcesses((ArrayNode)getRoot(readContent(inputStream)));
+            Collection<Process> processesInfo = decodeProcesses((ArrayNode)getRoot(readContent(inputStream)));
+            //Add ordering
+            Order orderingParameters = additionalParameters.getOrderingInfo();
+            if (orderingParameters!=null && orderingParameters.isInitialized())
+                processesInfo.add(new Process("order", orderingParameters));
+            //Add pagination
+            Page pageParameters = additionalParameters.getPaginationInfo();
+            if (pageParameters!=null && pageParameters.isInitialized())
+                processesInfo.add(new Process("page", pageParameters));
+            //Return processes
+            return processesInfo.toArray(new Process[processesInfo.size()]);
         } catch (Exception e) {
             throw new WebApplicationException(e);
         }
     }
 
-    private Process[] decodeProcesses(ArrayNode sources) throws Exception {
-        int size = sources!=null ? sources.size() : 0;
-        Process[] processesData = new Process[size];
-
-        for (int i=0; i<size; i++)
-            processesData[i] = decodeProcess(sources.get(i));
-
+    private Collection<Process> decodeProcesses(ArrayNode sources) throws Exception {
+        Collection<Process> processesData = new LinkedList<>();
+        for (JsonNode source : sources)
+            processesData.add(decodeProcess(source));
         return processesData;
     }
 
@@ -56,7 +70,7 @@ public class ProcessesProvider extends JsonProvider implements MessageBodyReader
             Type paramsType = processor.getParametersType();
             return paramsType==null ? JSONUtils.decode(source.toString(), Process.class) : JSONUtils.decode(source.toString(), Process.class, paramsType);
         } else
-            return null;
+            throw new BadRequestException("Requested process '"+name+"' not found");
     }
 
 }
